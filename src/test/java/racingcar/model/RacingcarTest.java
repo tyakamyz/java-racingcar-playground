b9b6553f11a386ead076434691348a6958973caf
tyakamyz <tyakamyz@naver.com>
2022-03-11 17:41:18 +0900

    test: 예외 처리 테스트 추가

--- src/test/java/racingcar/model/RacingcarTest.java
+++ src/test/java/racingcar/model/RacingcarTest.java
@@ -1,18 +1,29 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingcarTest {
 
+    @DisplayName("문자열을 Racingcar 객체로 변경")
     @Test
     public void inputStringToRacingcars(){
         assertThat(Racingcar.inputStringToRacingcars("pobi,crong,honux"))
                 .usingRecursiveComparison()
                 .isEqualTo(Arrays.asList(new Racingcar("pobi"), new Racingcar("crong"),new Racingcar("honux")));
+    }
+
+    @DisplayName("문자열 유효성 체크")
+    @Test
+    public void inputStringToRacingcars_validationCheck(){
+        assertThatThrownBy(() -> Racingcar
+                .inputStringToRacingcars("pobi,recrong,honux"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
