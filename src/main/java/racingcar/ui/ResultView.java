d002d68bc4558a2748674b178fc6404c917cfbd4
sihyun <sihyun.park@skylabs.io>
2022-03-14 18:30:27 +0900

    feat: UI 관련 클래스 생성
    * InputView: 입력 UI
    * ResultView: 출력 UI
    * Application: main 함수

---
+++ src/main/java/racingcar/ui/ResultView.java
@@ -0,0 +1,13 @@
+package racingcar.ui;
+
+public class ResultView {
+
+    public static void startMessage(){
+        System.out.println("실행 결과");
+    }
+
+    public static void endMessage(String winner){
+        System.out.println(winner + "이(가) 최종 우승했습니다.");
+    }
+
+}
